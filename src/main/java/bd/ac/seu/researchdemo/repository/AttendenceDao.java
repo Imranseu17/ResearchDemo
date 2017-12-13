cd25529e65458b69da2cd69464bea308fdce37a0
imran <imran1995bd@gmail.com>
2017-12-14 01:31:07 +0600

    Student part add

--- src/main/java/bd/ac/seu/researchdemo/repository/AttendenceDao.java
+++ src/main/java/bd/ac/seu/researchdemo/repository/AttendenceDao.java
@@ -14,9 +14,7 @@ import java.util.List;
 public interface AttendenceDao extends CrudRepository<Attendance,Integer> {
     public List<Attendance> findBySectionId(int id);
     public List<Attendance> findByAttendenceStatusAndSection_IdAndStudentStudentId(AttendenceStatus attendendenceStatus, int sectionId, String studentId);
-    public List<Attendance> findByDateTimeAndSectionIdAndStudentStudentId(LocalDateTime dateTime,int sectionId,String studentId);
-
-    public List<Attendance> findByDateTime(LocalDateTime dateTime);
+    public List<Attendance> findByStudentStudentIdAndSectionId(String Sid,int SecId);
 
 
 }
